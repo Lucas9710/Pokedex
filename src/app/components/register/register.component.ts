c22ebdece892ba8734faccbf6e5b63caa3eb510f
Marian Note <mariano_rs11@hotmail.com>
2022-11-01 01:55:05 -0300

    login sin funciones

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,7 +6,7 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../login/login.component.scss']
 })
 
 export class RegisterComponent implements OnInit {
